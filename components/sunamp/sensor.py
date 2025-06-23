2ed5e22db6ef5ca2a646b5cc55fbc668cfdb378b
wawyed <wawyed@gmail.com>
2025-06-23 20:04:29 +0100

    Add sunamp component

--- components/sunamp/sensor.py
+++ components/sunamp/sensor.py
@@ -20,7 +20,7 @@ CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
         cv.Required(CONF_SENSORS): cv.ensure_list(
             sensor.SENSOR_SCHEMA.extend(
                 {
-                    cv.Required(CONF_INDEX): cv.string,
+                    cv.Required("sunamp_id"): cv.string,
                 }
             )
         ),
